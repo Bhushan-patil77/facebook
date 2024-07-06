60eb03f28095ea4fbca531572d1150cb2dd8bfca
Bhushan-patil77 <bhushanravindrapatil77@gmail.com>
2024-07-06 17:08:53 +0530

    signup DOB dropdowns

--- src/Login.jsx
+++ src/Login.jsx
@@ -88,12 +88,12 @@ function Login({setisloggedin,setpopupshow}) {
 
         <div className='w-[40%] h-full flex  justify-center pt-[18%]'>
           <div className='w-[70%] flex flex-col gap-4'>
-          <h1 className='text-[#0866ff] text-6xl font-bold  '>facebook</h1>
-          <p className='text-2xl font-semibold '>Facebook helps you connect and share with the people in your life.</p>
+          <h1 className='text-[#0866ff] text-6xl font-bold  fade-in delay-1'>facebook</h1>
+          <p className='text-2xl font-semibold fade-in delay-2'>Facebook helps you connect and share with the people in your life.</p>
           </div>
         </div>
 
-        <div className="flex flex-col items-center gap-3 bg-white rounded-xl w-[30%] p-4  ">
+        <div className="flex flex-col items-center gap-3 bg-white rounded-xl w-[30%] p-4  fade-in delay-3">
           <form className='w-full flex flex-col gap-3' onSubmit={handleSubmit}>
 
             <input className='border borderbalck w-full h-[50px] text-lg pl-3 rounded-lg outline-[1px] outline-[#0866ff]' type="text" placeholder='Email address or phone number' value={email} onChange={(e)=>{setEmail(e.target.value)}} />
